f10d62cbb71e8fa5576752639f81f430e0b76ccf
Manharankaur01 <manharan.kaur01@sarasai.org>
2024-10-14 14:44:55 +0530

    Modified song management

--- Songs_Management.py
+++ Songs_Management.py
@@ -1,132 +1,141 @@
-#Will create 2 classes: song and artist. The object of song will be used as a data member for artist
+### This programm allow developers to interact with a database of songs efficiently.
 
-class Song():
-    def __init__(self,name,artist,album,genre,duration):
-        name = formatName(name)
-        artist = formatName(artist)
-        album = formatName(album)
-        genre = formatName(genre)
-        duration = formatName(duration)
-
-        self.name = name
+## Song class to store single song's attributes
+class Song:
+    # Constructor to define song data
+    def __init__(self,title,artist,album,genre,duration):
+        self.title = title
         self.artist = artist
         self.album = album
         self.genre = genre
         self.duration = duration
+
+    # Return song attributes as dictionary
+    def song_dict(self):
+        return {
+            "title":self.title,
+            "artist":self.artist,
+            "album":self.album,
+            "genre":self.genre,
+            "duration":self.duration
+        }
     
-class Artist():
-    def __init__(self,name,song):
-        name = formatName(name)
-        self.name = name
-        self.songs = {song.name:song}
-    
-    #Add song to the artist's collection
-    def add_song(self,song):
 
-        self.songs.update({str(song.name) : song})
+## Database class for managing playlists and songs
+class database:
+    # Constructor to empty playlist
+    def __init__(self):
+        self.playlist = {}
 
-
-'''Defining all the required functions'''
-
-def input_choice():
-    '''
-    Documentation for the input_choice()
-    Takes an input and makes sure exceptions are handled
-    '''
-    count =0
-    while(count<5): #Will tolerate maximum of 5 incorrect entries
+    # Load songs from a specified file to the playlist
+    def Load_File(self,file_name):
+        print(file_name)
         try:
-            choice = int(input("\nChoose an option (1,2 or 3): "))
-        except:
-            print("Invalid input!")
+            with open(file_name,"r") as file:
+                for line in file:
+                    song_attributes = line.strip().split(',')
+                    if len(song_attributes) == 5:
+                        # Create a Song object
+                        song = Song(song_attributes[0],song_attributes[1],song_attributes[2],song_attributes[3],song_attributes[4])
+                        # Add the song to the playlist under the artist's name
+                        if song.artist not in self.playlist:
+                            self.playlist[song.artist] = []
+                        self.playlist[song.artist].append(song)
+            print(f"Songs loaded from {file_name}.\n")
+        except FileNotFoundError:
+            print("File not found!\nTaking back to main menu...\n")
+        return
+
+    # Display the database in the formatted form
+    def display(self):
+        print("Song Database:\n")
+        print("Title".ljust(30),"Artist".ljust(30),"Genre".ljust(20))
+        print("="*80)
+        for artist in self.playlist:
+            songs = self.playlist[artist]
+            for s in songs:
+                print(s.title.ljust(30), s.artist.ljust(30), s.genre.ljust(20))
+        return
+
+    # Delete the song based on artist and song title from the database
+    def delete(self,artist,title):
+        if artist in self.playlist:
+            for song in self.playlist[artist]:
+                if song.title == title:
+                    self.playlist[artist].remove(song)
+                    if not self.playlist[artist]:
+                        del self.playlist[artist]
+                    return True
+            print("Song not found")
         else:
-            return choice
-        finally:
-            count += 1
-    if count>=5 :
-        print("Too many incorrect inputs, please try after sometime.")
-        exit()
-
-def formatName(name):
-    '''
-    Documentation for formatName()
-    This function will remove any leading or trailing whitespaces.
-    It also converts the name to tite case.
-    '''
-    #Removing any single or double quotes
-    name = name.strip()
-    name = name.strip("'")
-    name = name.strip('"')
-    name = name.strip()
-    return name.title()
-
-
-'''All required functions defined'''
-
-#Extracting information from file
-f = open("Songs.txt","r")
-f.readline() #As the document starts with a header
-playlist = {}
-
-#Formatting and storing the information in the dictionary
-for line in f:
-    info = list(line[:-1].split(','))
-    if(len(info)==5):
-        song = Song(info[0],info[1],info[2],info[3],info[4])
-        #If the artist is not in the playlist, add the artist and the song
-        if song.artist not in playlist.keys():
-            artist = Artist(song.artist,song)
-            playlist[artist.name] = artist
-        #If the artist is already in the playlist, add the song to the artist's collection
+            print("Artist Not Found")
+        return False
+    
+    # Modify a song's attributes based on artist and title
+    def modify(self,artist,title):
+        if artist in self.playlist:
+            for song in self.playlist[artist]:
+                if song.title == title:
+                    print("Current Details:")
+                    print(song.song_dict())
+                    # Prompt for new attributes, allowing empty input to keep current values
+                    album = input("Enter new album (or press Enter to keep current): ")
+                    if album:
+                        song.album = album
+                    genre = input("Enter new genre (or press Enter to keep current): ")
+                    if genre:
+                        song.genre = genre
+                    duration = input("Enter new duration (or press Enter to keep current): ")
+                    if duration:
+                        song.duration = duration
+                    return True
+            print("Song not found")
         else:
-            playlist[song.artist].add_song(song)
+            print("Artist Not Found")
+        return False
 
-f.close()
-count = 0
-while(1):
-    if count >4:
-      print("Too many unsuccessful attempts. Exiting the session\n")
-    
-    print("--- User Menu ---")
-    print("1. Search for a Song by Title")
-    print("2. Search for all Songs by an Artist")
-    print("3. Exit")
 
-    choice = input_choice()
 
-    if choice == 1:
-        status = False
-        song_name = input("Enter the song title: ")
-        song_name = formatName(song_name)
-        for artist,artist_obj in playlist.items():
-                for title,song in artist_obj.songs.items():
-                    if title == song_name:
-                        print(f"Found: {song.name} by {song.artist} ,Genre: {song.genre}, Album: {song.album}\n")
-                        status = True
-                        break
 
-        if status == False:
-            print("Song not found!\n")
-    
-    elif choice == 2:
-        artist_name = input("Enter the artist name: ")
-        artist_name = formatName(artist_name)
-        if artist_name not in playlist.keys():
-            print("Artist not found!\n")
+# Function to display Developer's menu->
+def menu():
+    print("Developer Menu:")
+    print("1. Load Song Data")
+    print("2. View Song Database")
+    print("3. Delete a Song")
+    print("4. Modify a Song")
+    print("5. Exit\n")
 
-        else:
-            artist_obj = playlist[artist_name]
-            print(f"Searching for songs by artist: {artist_obj.name}")
-            for title,song in artist_obj.songs.items():
-                print(f"Found: {title} (Genre: {song.genre}, Album: {song.album}, Duration: {song.duration})")
-            print("\n")
-            
-    
-    elif choice == 3:
-        print("Exiting... Goodbye!")
-        exit()
-    
-    else:
-        count +=1
-        print("Invalid choice. Please try again.")
-    
+
+db = database()
+while True:
+    menu() #displaying the menu
+    choice = int(input("Select an option: ")) # choosing an option
+    #match case for choice selection
+    match choice:
+        case 1: # Load song data
+            file_name = input("Enter the file name to load songs: ")
+            db.Load_File(file_name)
+
+        case 2: # View song database
+            db.display()
+        
+        case 3: # Delete a song
+            artist = input("Enter the artist's name of the song to delete: ")
+            name = input("Enter the title of the song to delete: ")
+            if db.delete(artist,name):
+                print(f"Deleted {name} by {artist} from the database.")
+        
+        case 4: # Modify a song
+            artist = input("Enter the artist's name of the song to modify: ")
+            title = input("Enter the title of the song to modify: ")
+            if db.modify(artist,title):
+                print(f"Modified {title} by {artist}.")
+        
+        case 5: # Exiting the programm
+            print("Exiting the application. Goodbye!\n")
+            break
+        
+        case _: # Handle invalid options
+            print("Please choose option from the menu!")
+    print()
